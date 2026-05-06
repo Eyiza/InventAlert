1168a0f493dd11c1beda3e57a311c5424e53f96f
Precious Michael <precious.michael2002@gmail.com>
2026-05-06 15:09:56 +0100

    setup: frontend

---
+++ inventAlert-frontend/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from 'react'
+import { Link } from 'react-router';
+import { toast } from 'react-toastify'
+
+const Home = () => {
+
+  return <div>Home</div>
+}
+
+export default Home
