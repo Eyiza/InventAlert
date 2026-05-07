99d8720303a3c1a84c67b45cb3b3f673fd005496
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-05-07 18:12:44 +0100

    feat: add EmailAlreadyExistsException for handling duplicate email scenarios

---
+++ identityService/src/main/java/com/inventalert/identityService/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.inventalert.identityService.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String email) {
+        super("Email already in use: " + email);
+    }
+}
