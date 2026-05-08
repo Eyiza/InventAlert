9680888888b103f20ddbaef0c3f072057bcdd0fe
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-05-08 20:32:03 +0100

    feat: add DynamicPropertySource for Kafka container configuration in AuthControllerTest

--- identityService/src/test/java/com/inventalert/identityService/controller/AuthControllerTest.java
+++ identityService/src/test/java/com/inventalert/identityService/controller/AuthControllerTest.java
@@ -13,6 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.testcontainers.containers.KafkaContainer;
@@ -37,9 +39,13 @@ class AuthControllerTest {
     static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8");
 
     @Container
-    @ServiceConnection
     static KafkaContainer kafka = new KafkaContainer(
             DockerImageName.parse("confluentinc/cp-kafka:7.6.0"));
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    }
 
     @Autowired MockMvc           mockMvc;
     @Autowired ObjectMapper      objectMapper;
