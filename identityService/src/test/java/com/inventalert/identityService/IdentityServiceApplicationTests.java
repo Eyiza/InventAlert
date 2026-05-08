510907d6c674a74139cadbb10d7925b63c3a80bf
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-05-08 18:44:40 +0100

    feat: enhance IdentityServiceApplicationTests with MySQL and Kafka containers

--- identityService/src/test/java/com/inventalert/identityService/IdentityServiceApplicationTests.java
+++ identityService/src/test/java/com/inventalert/identityService/IdentityServiceApplicationTests.java
@@ -2,9 +2,31 @@ package com.inventalert.identityService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class IdentityServiceApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8");
+
+    @Container
+    static KafkaContainer kafka = new KafkaContainer(
+            DockerImageName.parse("confluentinc/cp-kafka:7.6.0"));
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    }
 
     @Test
     void contextLoads() {
